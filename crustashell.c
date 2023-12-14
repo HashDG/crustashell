879fad87fad6c870eb9d6737a400d93c723c2692
Hippolyte Damay-glorieux <hippolyte.damayglorieux.etu@univ-lille.fr>
2023-12-14 16:31:48 +0100

    pipes + troubleshooting

--- crustashell.c
+++ crustashell.c
@@ -17,6 +17,7 @@
 
 void affiche_prompt(void);
 void execute_ligne_commande(char***, int, int);
+int lance_commande(int, int, const char*, char**);
 
 int main() {
   char*** command;
@@ -30,9 +31,8 @@ int main() {
     
     if (flag >= 0 && nb > 0) {
       if (strcmp(command[0][0], "exit") == 0) break;
-      
-      affiche(command);
-      execute_ligne_commande(command, 0, flag);
+
+      execute_ligne_commande(command, nb, flag);
       libere(command);
     }
   }
@@ -65,18 +65,70 @@ void execute_ligne_commande(commandes, nb, arriere_plan)
      int nb;
      int arriere_plan;
 {
-  int pid;
+  int lastpid;
   char* command, ** args;
+  int tube1[2], tube2[2], *tubes[2];
+  const int size = nb;
   
+  while (nb--) {    
+    command = **commandes;
+    args = *(commandes++);
+
+    if ((size-nb)%2==0) {
+      pipe(tube2);
+      tubes[0] = tube1;
+      tubes[1] = tube2;
+    } else {
+      pipe(tube1);
+      tubes[0] = tube2;
+      tubes[1] = tube1;
+    }
+    
+    lastpid = lance_commande(nb==size-1 ? 0 : tubes[0][0],
+		     nb == 0 ? 1 : tubes[1][1],
+		     command,
+		     args);
+  }
+
+  close(tube1[0]);
+  close(tube1[1]);
+  close(tube2[0]);
+  close(tube2[1]);
+  
+  if (!arriere_plan) waitpid(lastpid, NULL, 0);
+}
+
+int lance_commande(in, out, command, args)
+     int in;
+     int out;
+     const char* command;
+     char** args;
+{
+  int pid;
+
+  printf("I : %i, O : %i\n", in, out);
+
   pid = fork();
-  command = *commandes[nb];
-  args = commandes[nb];
 
   if (pid == 0) {
+    if (in != 0) {
+      close(0);
+      dup(in);
+      close(in);
+    }
+
+    if (out != 1) {
+      close(1);
+      dup(out);
+      close(out);
+    }
+
     execvp(command, args);
     printf("%s\n", strerror(errno));
     exit(EXIT_FAILURE);
+  } else {
+    if (in != 0) close(in);
+    if (out != 1) close(out);
   }
-
-  if (!arriere_plan) waitpid(pid, NULL, 0);
+  return pid;
 }
